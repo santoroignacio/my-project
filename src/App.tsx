5dbb80bfca501716dbe79207b3f8e461eabd5b62
Nacho <santoroignacio76@gmail.com>
2023-09-26 10:57:17 -0300

    Modifica App.tsx para mostrar Nav Bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Navbar } from './NavBar';
+import { RutaNoValida } from './RutaNoValida';
+import { useCallback, useEffect } from 'react'
+import { getProductos } from './state/productoSlice';
+import { useAppDispatch, useAppSelector } from './state/hooks';
+import { ProductosPage } from './components/ProductsPage';
+import { SingleProductoPage } from './components/SingleProductPage';
+import { CreateProductPage } from './components/CreateProductPage';
+import { EditProductPage } from './components/EditProductPage';
 
 function App() {
+
+  const dispatch = useAppDispatch()
+  const { productos } = useAppSelector(state => state.producto)
+
+  const initApp = useCallback(async () => {
+    await dispatch(getProductos())
+  }, [dispatch])
+
+  useEffect(() => {
+    initApp()
+  }, [])
+
   return (
     <div className="App">
-      
+      <BrowserRouter>
+
+        <Navbar />
+
+        <Routes>
+
+          <Route index element={<ProductosPage productoArray={productos} />}></Route>
+
+          <Route path='/:id' element={<SingleProductoPage />}></Route>
+          <Route path='/createProductPage' element={<CreateProductPage />}></Route>
+          <Route path='/editProductPage/:id' element={<EditProductPage />}></Route>
+
+          <Route path='*' element={<RutaNoValida />}></Route>
+
+
+        </Routes>
+      </BrowserRouter>
+
     </div>
   );
 }

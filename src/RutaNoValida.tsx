0b90031866d28bd4db3c6d4f61ccbf5d48a92973
Nacho <santoroignacio76@gmail.com>
2023-09-26 10:14:48 -0300

    Crea RutaNoValida.tsx

---
+++ src/RutaNoValida.tsx
@@ -0,0 +1,12 @@
+import './RutaNoValida.css'
+
+
+export const RutaNoValida: React.FC = () => {
+    return (
+        <div className="RutaNoValida">
+            <div className="jumbotron">
+                <h3>Error 404: Ruta no válida</h3>
+            </div>
+        </div>
+    )
+}
